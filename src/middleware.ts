5e5abf8fa0c6c0cd8d71c94fc70f082bf3b39a17
Arturo Mejia <armejia02@gmail.com>
2025-04-10 18:26:33 -0600

    feat(middleware): enhance authentication middleware for protected routes
    - Integrated route matching to identify protected routes.
    - Updated the middleware to redirect unauthenticated users to the sign-in page when accessing protected routes.
    - Maintained existing functionality while improving user authentication flow.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,24 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+});
 
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)", 
-    "/", 
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
     "/(api|trpc)(.*)",
     "/login(.*)",
     "/sign-in(.*)",
     "/sign-up(.*)",
-    "/sso-callback(.*)"
+    "/sso-callback(.*)",
   ],
 };
